14e806c21a90244495e4a4b7ba0a137a50577098
benjaminmico <benjamin.mico@gmail.com>
2021-08-25 08:30:38 +0200

    feat(navigation): update stack

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import {StatusBar} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {Provider} from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
+import configureStore from './store/configureStore';
 
 type StackParams = {
   Home: undefined;
@@ -15,19 +18,25 @@ type StackParams = {
 const Stack = createStackNavigator<StackParams>();
 
 const App: React.FC = () => {
+  const store = configureStore();
+
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="light-content" />
-      <Stack.Navigator initialRouteName="Tab">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <PaperProvider>
+        <NavigationContainer>
+          <StatusBar barStyle="light-content" />
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </Provider>
   );
 };
 
